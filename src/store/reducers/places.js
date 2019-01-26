dbfd35524e489e185ea69830a9c762ec4e714221
Mario Boskovski <MarioBoskovskiSkopje>
2019-01-26 12:50:49 +0100

    first commit

---
+++ src/store/reducers/places.js
@@ -0,0 +1,16 @@
+const initalState = {
+  places: [],
+  selectedPlace: null,
+}
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case value:
+      break
+
+    default:
+      return state
+  }
+}
+
+export default reducer
